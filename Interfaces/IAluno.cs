63e15649323605f41ae68f1d3d649d39e68d3ab1
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-09-01 18:05:31 -0300

    Estrutura de Pastas Finalizada

---
+++ Interfaces/IAluno.cs
@@ -0,0 +1,21 @@
+﻿using API_Boletim.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno a);
+
+        List<Aluno> LerTodos();
+
+        Aluno BuscarPorId(int id);
+
+        Aluno Alterar(Aluno a);
+
+        Aluno Excluir(Aluno a);
+    }
+}
